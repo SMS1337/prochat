c8b2852c267f94f1c592f18abea6f2a0d19bae34
dylanROBLOX <virtualdy.lan5@gmail.com>
2015-11-06 15:36:33 -0500

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,5 @@
+local settings = {}
+	settings["nicknameLocked"] = true -- If set to true, nicknames are only for admins.
+	settings["feedback"] = false --Gives you output spam on everything that happens, good for debugging.
+	settings["extraadmins"] = {"h5s";"olympicpickle";"sms1337";"yayzman23"} --people who get admin badge, defaults to contributers
+return settings
