110b45ca64b962da9baf6afbb8630d28bd5e9cbd
dylanROBLOX <virtualdy.lan5@gmail.com>
2015-11-06 15:36:05 -0500

    Create Introduction.lua

---
+++ Introduction.lua
@@ -0,0 +1,21 @@
+--[[
+	This is a folder of neccesary files to get ProChat working in your server.
+	Don't modify it.
+		https://github.com/dylanROBLOX/prochat
+		v1.1
+	-----------------------------------------------------------------
+	|																|
+	|					/!\ LICENSE AND AGREEMENT					|
+	|		-Only re-release this if you have made changes. 		|
+	|		-By editing scripts we won't help fix your issues.		|
+	|																|
+	-----------------------------------------------------------------
+	
+	--
+	Contributors: 
+		-h5s
+		-SMS1337
+		-YAYZMAN23
+		
+		Distributed as OlympicPickle. This is the official release.
+--]]
