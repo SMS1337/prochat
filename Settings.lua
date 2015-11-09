0f1a17216d4e7362325b5823718e6c0489c8f7c8
dylanROBLOX <virtualdy.lan5@gmail.com>
2015-11-09 14:49:47 -0500

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,6 +1,7 @@
 local settings = {}
 	settings["nicknameLocked"] = true 										   -- If set to true, nicknames are only for admins.
 	settings["feedback"]       = false                                         -- Gives you output spam on everything that happens, good for debugging.
-	settings["extraadmins"]    = {"h5s";"olympicpickle";"sms1337";"yayzman23"} -- people who get admin badge, defaults to contributers
+	settings["extraadmins"]    = {"h5s";"olympicpickle";"player1";"yayzman23"} -- people who get admin badge, defaults to contributers
 	settings["robloxadmins"]   = true                                          -- Official ROBLOX admins will also get gold text if true.
+	settings["activityLog"]    = true										   -- Will say when a player leaves and joins.
 return settings
